095abdad19644b4e4dd52cfd0bb80c87a85bbc7f
Masjid Abs <masjid@uinkediri.ac.id>
2026-08-10 15:42:39 +0700

    Use browser-compatible Kediri prayer schedule

--- script.js
+++ script.js
@@ -23,10 +23,10 @@
   const prayerSchedule = document.querySelector('[data-prayer-schedule]');
   if (prayerSchedule) {
     const prayerDate = document.querySelector('[data-prayer-date]');
-    fetch('/api/prayer-times')
+    fetch('https://www.muslimkita.id/api/jadwal-sholat/v1/kediri?metode=kemenag')
       .then((response) => { if (!response.ok) throw new Error('Jadwal tidak tersedia'); return response.json(); })
       .then((response) => {
-        const schedule = response?.schedule;
+        const schedule = response?.jadwal;
         const times = [['Subuh', schedule?.subuh], ['Zuhur', schedule?.dzuhur], ['Asar', schedule?.ashar], ['Maghrib', schedule?.maghrib], ['Isya', schedule?.isya]];
         if (times.some(([, time]) => !time)) throw new Error('Format jadwal tidak tersedia');
         prayerSchedule.innerHTML = times.map(([name, time]) => `<div class="prayer-time"><span>${name}</span><strong>${time}</strong></div>`).join('');
